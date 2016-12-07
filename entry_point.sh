b55eab90762ab709f9118b8ee90cc1d181a01104
Shayne Clausson <shayne.clausson@stelligent.com>
2016-12-07 22:32:11 +0100

    initial commit

---
+++ entry_point.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# https://raw.githubusercontent.com/SeleniumHQ/docker-selenium/master/StandaloneFirefox/entry_point.sh
+
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+xvfb-run --server-args="$DISPLAY -screen 0 $GEOMETRY -ac +extension RANDR" &
+NODE_PID=$!
+
+trap shutdown SIGTERM SIGINT
+
+exec "$@"
